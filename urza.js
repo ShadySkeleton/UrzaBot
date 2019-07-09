ae8a7f25f30efcc5dcd8d6beac05b2b0a963255f
Tom Schilling <Tom.Schilling.89@gmail.com>
2019-07-09 19:25:48 +0200

    Added config file to obscure bot token

--- urza.js
+++ urza.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js');
- const client = new Discord.Client();
+const client = new Discord.Client();
+const CONFIG = require('./config.json');
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
+
+ if (msg.content.startsWith('!wants')) {
  msg.reply('pong');
  }
+
  });
 
-client.login('NTk4MTYyMzYxNTIxNTM3MDQ0.XSSomg.TUdg2AO3bHWeMWEogLo2MGBCxyg');
+client.login(CONFIG.token);
